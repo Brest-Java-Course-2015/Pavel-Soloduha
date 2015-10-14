e6fb477a07b109f16dbfbb08c2782e351d12bd52
Pavel-Soloduha <shadowofclown@gmail.com>
2015-10-14 13:30:46 +0300

    Create own copy of Sample-User-Management

---
+++ app-core/src/main/java/com/epam/brest/course2015/domain/User.java
@@ -0,0 +1,77 @@
+package com.epam.brest.course2015.domain;
+
+import java.util.Date;
+
+/**
+ * Created by pavel on 14.10.15.
+ */
+public class User {
+
+    private Integer userId;
+
+    private String login;
+
+    private String password;
+
+    private Date createdDate;
+
+    public User() {
+    }
+
+    public User(Integer userId, String login, String password, Date createdDate) {
+        this.userId = userId;
+        this.login = login;
+        this.password = password;
+        this.createdDate = createdDate;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public static enum UserFields {
+
+        USER_ID ("userId"),
+        LOGIN ("login"),
+        PASSWORD ("password"),
+        CREATED_DATE ("createdDate");
+
+        private UserFields(String value){
+            this.value = value;
+        }
+
+        private final String value;
+
+        public String getValue(){return value;}
+
+    }
+
+}
---
+++ app-dao-api/src/main/java/com/epam/brest/course2015/dao/UserDao.java
@@ -0,0 +1,24 @@
+package com.epam.brest.course2015.dao;
+
+import com.epam.brest.course2015.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by pavel on 14.10.15.
+ */
+public interface UserDao {
+
+    public List<User> getAllUsers();
+
+    public User getUserById(Integer userId);
+
+    public User getUserByLogin(String login);
+
+    public Integer addUser(User user);
+
+    public void updateUser(User user);
+
+    public void deleteUser(Integer userId);
+
+}
