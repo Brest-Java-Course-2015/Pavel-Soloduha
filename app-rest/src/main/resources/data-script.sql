eae5ca2936c7de1d907142fc4a56714548fac509
Pavel-Soloduha <shadowofclown@gmail.com>
2015-11-08 16:06:47 +0300

    change SQL-queries for my project

--- app-rest/src/main/resources/data-script.sql
+++ app-rest/src/main/resources/data-script.sql
@@ -1,2 +1,2 @@
-INSERT INTO USER (userid, login, password, updatedDate) VALUES (1, 'userLogin1', 'userPassword1', TO_TIMESTAMP('14/10/2015 20:30:00', 'DD/MM/YYYY HH:MI:SS'));
-INSERT INTO USER (userid, login, password, updatedDate) VALUES (2, 'userLogin2', 'userPassword2', TO_TIMESTAMP('14/10/2015 21:35:00', 'DD/MM/YYYY HH:MI:SS'));
+INSERT INTO DETAIL (detailid, detailname) VALUES (1, 'detailName1');
+INSERT INTO DETAIL (detailid, detailname) VALUES (2, 'detailName2');
